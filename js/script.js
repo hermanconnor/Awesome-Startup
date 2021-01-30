b90d0f7d1f50d7a0b85338d8b0172b2caa5fc969
Herman <hermanconnor11@gmail.com>
2021-01-29 22:28:51 -0500

    Whatever

--- js/script.js
+++ js/script.js
@@ -11,20 +11,55 @@ async function fetchData(url) {
   return data;
 }
 
-fetchData('https://randomuser.me/api/?results=12').then(data =>
-  console.log(data)
-);
+// fetchData('https://randomuser.me/api/?results=12').then(data =>
+//   console.log(data)
+// );
 
-displayPage();
+displayEmployees();
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-function displayPage() {
+function displayEmployees() {
+  createForm();
+
+  let employees;
+  fetchData('https://randomuser.me/api/?results=100&nat=us').then(
+    data => {
+      employees = data.results;
+      getEmployees(employees, 1);
+    }
+
+    // data.results.forEach(employee => {
+    //   htmlString = `
+    //     <div class="card">
+    //       <div class="card-img-container">
+    //         <img class="card-img" src=${employee.picture.large} alt="profile picture">
+    //       </div>
+    //       <div class="card-info-container">
+    //         <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+    //         <p class="card-text">${employee.email}</p>
+    //         <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+    //       </div>
+    //     </div>
+    //   `;
+    //   gallery.insertAdjacentHTML('beforeend', htmlString);
+    // })
+  );
+}
+
+function getEmployees(data, page) {
+  // Calculate the index for first and last employee to display
+  const startIndex = page * 12 - 12;
+  const endIndex = page * 12;
+
+  // Set the variable used to construct/append DOM elements
   const gallery = document.querySelector('.gallery');
   let htmlString;
 
-  fetchData('https://randomuser.me/api/?results=12&nat=us').then(data =>
-    data.results.forEach(employee => {
+  for (let i = 0; i < data.length; i++) {
+    let employee = data[i];
+    if (i >= startIndex && i < endIndex) {
+      // Create the DOM elements
       htmlString = `
         <div class="card">
           <div class="card-img-container">
@@ -37,9 +72,11 @@ function displayPage() {
           </div>
         </div>           
       `;
+
+      // Insert the created elements to the page
       gallery.insertAdjacentHTML('beforeend', htmlString);
-    })
-  );
+    }
+  }
 }
 
 function createForm() {
@@ -58,9 +95,11 @@ function createForm() {
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-const searchInput = document.querySelector('.search-input');
-const searchBtn = document.querySelector('.search-submit');
 
-searchInput.addEventListener('keyup', e => {
-  const inputText = e.target.value;
-});
+// searchInput.addEventListener('keyup', e => {
+//   const inputText = e.target.value;
+
+//   if (inputText !== '') {
+//     console.log(inputText);
+//   }
+// });
